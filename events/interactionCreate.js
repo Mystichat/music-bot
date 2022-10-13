92ee2a8d8bfec812676d72e9ee39847b340801da
Mystichat <87788556+Mystichat@users.noreply.github.com>
2022-10-13 10:30:18 -0400

    fix: minor error in interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 module.exports = async (client, interaction) => {
     if (interaction.type === Discord.InteractionType.ApplicationCommand) {
-        let command = require(`../commmands/${interaction.commandName}`)
+        let command = require(`../commands/${interaction.commandName}`)
         command.run(client, interaction, command.options)
     }
 }
